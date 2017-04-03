d566c2a240751cf6a83a2dd133087de73a0a5dfc
Stone <generalms@hotmail.com>
2017-04-03 02:25:46 -0500

    Fixed MST bug and implemented navigation to waypoint

--- MST.py
+++ MST.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Node:
 
 	def __init__(self,node_id,pos):
@@ -6,24 +9,28 @@ class Node:
 		self.neighbours={}
 
 	def add_neighbor(self,neighbour,cost):
+
 		self.neighbours[neighbour] = cost
 
 	def get_weight(self,neighbor):
 		return self.neighbours[neighbour]
 
+	def get_neighbors(self):
+		return self.neighbours.keys()
+
 	def get_cost(self,node_id):
 		if self.id == node_id:
 			return 0
-		else if node_id not in self.neighbours.keys
+		elif node_id not in self.neighbours.keys():
 			return 999999999
 		else:
-			return self.neighbours[neighbour]
+			return self.neighbours[node_id]
 
-	def get_pos:
+	def get_pos(self):
 		return self.pos
 
 
-class Graph
+class Graph:
 
 	def __init__(self):
 		self.node_list = {}
@@ -31,24 +38,30 @@ class Graph
 
 
 	def add_node(self,node_id,pos):
+		#print(node_id)
 		self.num_vertices = self.num_vertices + 1
 		new_node = Node(node_id,pos)
-		self.node_list[node] = new_node
+		self.node_list[node_id] = new_node
 
 
 
-	def add_edge(self,node1,node2,cost):
-		if node1 not in self.node_list:
-			self.add_node(node1)
+	def add_edge(self,node1,node2):
+		'''if node1 not in self.node_list.keys():
+			self.add_node(node1,(0,0))
 
-		if node2 not in self.node_list:
-			self.add_node(node2)
-
-		self.node_list[node1].add_neighbor(node2,cost)
+		if node2 not in self.node_list.keys():
+			self.add_node(node2,(0,0))'''
+		
+		cost = self.calculate_edge_cost(self.node_list[node1].get_pos(),self.node_list[node2].get_pos())
+		#print(cost)
 		self.node_list[node2].add_neighbor(node1,cost)
+		self.node_list[node1].add_neighbor(node2,cost)
 
+	def calculate_edge_cost(self,pos1,pos2):
+		return math.sqrt(math.pow((pos1[0]-pos2[0]),2) + math.pow((pos1[1]-pos2[1]),2))
+	
 	def get_node(self,node_id):
-		if node_id in node_list:
+		if node_id in self.node_list:
 			return self.node_list[node_id]
 		else:
 			return None
@@ -56,44 +69,140 @@ class Graph
 	def get_nodes(self):
 		return self.node_list.keys()
 
+	def print_graph(self):
+		for n in self.get_nodes():
+			node  = self.get_node(n)
+			print(n + ":  ")
+			for i in node.get_neighbors():
+				print(i +":" + str(node.get_cost(i))  +",")
+			#print("\n")
 
-def min_neighbor(node,edge_list):
+
+
+
+
+
+
 	
-	prev_min = 999999999999999999
-	win_node = None
-	for n in edge_list.keys
-		if(node!=n):
-			if(edge_list[n]<prev_min):
-				prev_min = edge_list[n]
-				win_node = n
-	return win_node
 
 
-def compute_MST(G):
-	new_graph = Graph()
-
-	for n in adjacency_matrix.keys()
-		new_graph.add_node(n)
-
-	adjacency_matrix = compute_adjacency_matrix(G)
-
-	for n in adjacency_matrix.keys()
-		win_node = min_neighbor(n,adjacency_matrix[n])
-		new_graph.add_neighbor(n,win_node)
-
-	return new_graph		
 
 
-def compute_adjacency_matrix(Graph):
-	matrix={}
-	for i in Graph.get_nodes
-		node = Graph.get_node(j)
-		row_matrix={}
-		for j in Graph.get_nodes
-			cost = node.get_cost(j)
-			row_matrix[j] = cost
-		matrix[i] = row_matrix
-	return matrix 
+
+
+
+
+
+
+class MST:
+	
+	def __init__(self,graph):
+		self.graph = graph
+
+	def compute_MST(self):
+		new_graph = Graph() 
+		adjacency_matrix = self.compute_adjacency_matrix()
+		print("Computing MST...")
+		i=0
+
+		for n in adjacency_matrix.keys():
+			new_graph.add_node(n,self.graph.get_node(n).get_pos())
+		
+		for n in adjacency_matrix.keys():
+
+			if(i<self.graph.num_vertices-1):
+				win_node = self.min_neighbor(n,adjacency_matrix[n])
+				#new_graph.add_node(win_node,self.graph.get_node(win_node).get_pos())
+				row = adjacency_matrix[win_node]
+				row.pop(n)
+				adjacency_matrix[win_node] = row
+				#print(adjacency_matrix)
+				#print(n)
+				#print(win_node)
+				new_graph.add_edge(n,win_node)
+				#new_graph.print_graph()
+				#print("__________________________")
+				i=i+1
+		return new_graph
+
+	def min_neighbor(self,node,edge_list):
+	
+		prev_min = 999999999999999999
+		win_node = None
+		for n in edge_list.keys():
+			if(node!=n):
+				if(edge_list[n]<prev_min):
+					prev_min = edge_list[n]
+					win_node = n
+		return win_node
+
+	def compute_adjacency_matrix(self):
+		matrix={}
+		for i in self.graph.get_nodes():
+			#print(i)
+			node = self.graph.get_node(i)
+			row_matrix={}
+			for j in self.graph.get_nodes():
+				cost = node.get_cost(j)
+				row_matrix[j] = cost
+			matrix[i] = row_matrix
+		return matrix
+
+def calculate_edge_cost(pos1,pos2):
+		return math.sqrt(math.pow((pos1[0]-pos2[0]),2) + math.pow((pos1[1]-pos2[1]),2))
+
+
+def main():
+	#print(calculate_edge_cost((5,2),(3,2)))
+	#print(calculate_edge_cost((3,2),(5,2)))
+
+	g = Graph()
+
+	g.add_node('A',(2,3))
+	g.add_node('B',(4,5))
+	g.add_node('C',(2,6))
+	g.add_node('D',(3,5))
+	g.add_node('E',(6,5))
+
+	g.add_edge('A','B')
+	g.add_edge('A','C')
+	g.add_edge('A','D')
+	g.add_edge('A','E')
+
+	g.add_edge('B','C')
+	g.add_edge('B','D')
+	#g.add_edge('B','A')
+	g.add_edge('B','E')
+
+	#g.add_edge('D','A')
+	g.add_edge('D','C')
+	#g.add_edge('D','B')
+	g.add_edge('D','E')
+
+	#g.add_edge('C','B')
+	#g.add_edge('C','A')
+	#g.add_edge('C','D')
+	g.add_edge('C','E')
+
+	#g.add_edge('E','B')
+	#g.add_edge('E','A')
+	#g.add_edge('E','D')
+	#g.add_edge('E','C')
+
+	
+
+	g.print_graph()
+
+	mst = MST(g)
+	new_graph = mst.compute_MST()
+	new_graph.print_graph()
+
+
+
+if __name__ == "__main__":
+
+
+    main()
 
 
 
--- simulator.py
+++ simulator.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+from MST import *
+import math
 
 
 
@@ -16,10 +18,11 @@ screenColor = (255,255,255)
 droneSprite = pygame.image.load('drone.png')
 crashed = False
 
-def init_waypoint_list():
+def init_waypoint_list(graph):
 	lst = []
-	for i in range(0,30):
-		lst+= [(random.randint(0,800),random.randint(0,600))]
+	for n in graph.get_nodes():
+		node = graph.get_node(n)
+		lst+=[node.get_pos()]
 	return lst
 
 def init_poly_list(poly_sides,num_poly):
@@ -32,7 +35,7 @@ def init_poly_list(poly_sides,num_poly):
 	return poly_list
 
 def drone(x,y):
-	gameDisplay.blit(droneSprite,(x,y))
+	gameDisplay.blit(droneSprite,(x-30,y-30))
 
 def draw_waypoint(pos):
 	pygame.draw.circle(gameDisplay,(0,0,255),pos,6)
@@ -46,22 +49,95 @@ def draw_map(wapoint_list,polygon_list):
 
 	for polygon in polygon_list:
 		draw_polygon(polygon)
+def draw_line(point1,point2):
+	pygame.draw.line(gameDisplay, (0,255,0), [point1[0], point1[1]], [point2[0],point2[1]], 5)
+
+def draw_graph(graph):
+
+	for n in graph.get_nodes():
+		node = graph.get_node(n)
+		draw_waypoint(node.get_pos())
+		for neighbor_id in node.get_neighbors():
+			neighbour_node = graph.get_node(neighbor_id)
+			draw_line(node.get_pos(),neighbour_node.get_pos())
+
+def create_graph():
+	g = Graph()
+
+	g.add_node('A',(300,500))
+	g.add_node('B',(150,400))
+	
+	g.add_node('D',(50,50))
+	g.add_node('E',(400,300))
+	g.add_node('C',(400,100))
+
+	g.add_edge('A','B')
+	g.add_edge('A','C')
+	g.add_edge('A','D')
+	g.add_edge('A','E')
+
+	g.add_edge('B','C')
+	g.add_edge('B','D')
+	g.add_edge('B','A')
+	g.add_edge('B','E')
+
+	g.add_edge('D','A')
+	g.add_edge('D','C')
+	g.add_edge('D','B')
+	g.add_edge('D','E')
+
+	g.add_edge('C','B')
+	g.add_edge('C','A')
+	g.add_edge('C','D')
+	g.add_edge('C','E')
+
+	g.add_edge('E','B')
+	g.add_edge('E','A')
+	g.add_edge('E','D')
+	g.add_edge('E','C')
+
+	return g
 
 
 
 
 
 
-way_lst = init_waypoint_list()
+
+
+
+
+
+
+
 poly_list = init_poly_list(5,2)
+graph = create_graph()
+way_lst = init_waypoint_list(graph)
 
-drone_x=0
-drone_y=0
+print(way_lst)
+
+drone_x=way_lst[0][0]
+drone_y=way_lst[0][1]
+#drone_y=300-30
+#drone_x=400-30
+#print((drone_x-20,drone_y))
+
+counter = 1
 def update_pos(drone_x,drone_y,speedX,speedY):
 	drone_x+=speedX
 	drone_y+=speedY
 	return drone_x,drone_y
 
+def move_to(current_x,current_y,desiredX,desiredY,kp):
+	move_x = kp*(desiredX-current_x)
+	move_y = kp*(desiredY-current_y)
+	return move_x,move_y
+
+def is_pos_equal(pos1,pos2):
+	if(math.fabs((pos1[0]-pos2[0]))<=1 and math.fabs((pos1[1]-pos2[1]))<=1):
+		return True
+	else:
+		return False
 
 while not crashed:
 
@@ -73,9 +149,33 @@ while not crashed:
 	#pygame.draw.line(gameDisplay, (0,255,0), [0, 0], [50,30], 5)
 	#pygame.draw.circle(gameDisplay,(0,0,255),(500,500),6)
 	#pygame.draw.polygon(gameDisplay,(255,0,0),[(400,200),(400,400),(200,400),(200,200)])
-	draw_map(way_lst,poly_list)
+	#draw_map(way_lst,poly_list)
+	draw_graph(graph)
+	#draw_waypoint((120,250))
 	drone(drone_x,drone_y)
-	drone_x,drone_y = update_pos(drone_x,drone_y,2,0)
+
+	#print(drone_x)
+	#print(drone_y)
+
+	if counter<graph.num_vertices-1:
+		if is_pos_equal((drone_x,drone_y),way_lst[counter]):
+			print("Im here")
+			print((drone_x,drone_y))
+			counter+=1
+			print(counter)
+	destX = way_lst[counter][0]
+	destY = way_lst[counter][1]
+	print(destX,destY)
+
+	#drone_x,drone_y = update_pos(drone_x,drone_y,2,0)
+	move_x,move_y = move_to(drone_x,drone_y,destX,destY,0.05)
+	drone_x,drone_y = update_pos(drone_x,drone_y,move_x,move_y)
+
+
+	if(drone_x>=600):
+		mst = MST(graph)
+		graph = mst.compute_MST()
+		graph.print_graph()
 	pygame.display.update()
 	clock.tick(60)
 
